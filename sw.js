de1f12d25c5505e53224dddf2dcdc807e750e23f
Kristina P. Sinaga <150363044+PatternKPS@users.noreply.github.com>
2024-03-14 10:57:42 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const PRECACHE_LIST = [
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
-  "./img/android-chrome-512x512.png",
+  "./img/avatar-hux.jpg",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",

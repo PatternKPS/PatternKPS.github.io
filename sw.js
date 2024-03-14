6893e530a07eb1693bba342074f6c7a5b305de72
PatternKPS <150363044+PatternKPS@users.noreply.github.com>
2024-03-14 10:53:23 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ const PRECACHE_LIST = [
   "./js/hux-blog.min.js",
   "./js/snackbar.js",
   "./img/icon_wechat.png",
-  "./img/avatar-hux.jpg",
+  "./img/android-chrome-512x512.png",
   "./img/home-bg.jpg",
   "./img/404-bg.jpg",
   "./css/hux-blog.min.css",
